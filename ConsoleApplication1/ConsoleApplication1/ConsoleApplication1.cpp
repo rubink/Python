e8770d402c4641a6a43a9cc352b513d779426136
rubin <rubin@DESKTOP-DUBDVQL>
2017-03-05 15:23:58 -0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,9 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+int main() {
+	printf("Hello World!\n");
+	return 0;
+}
